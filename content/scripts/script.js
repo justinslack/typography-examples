df9b92dc387f0837709d6add03216f7f65ee4f72
Justin Slack <justin@newmedialabs.co.za>
2016-10-07 08:36:05 +0200

    Typekit example

---
+++ content/scripts/script.js
@@ -0,0 +1,32 @@
+// All jquery scripts
+$(document).ready(function ($) {
+
+//Script for tabs
+
+  $('.tab-controls').each(function(){
+    var $active, $content, $links = $(this).find('a');
+
+    $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+    $active.addClass('active');
+
+    $content = $($active[0].hash);
+
+    $links.not($active).each(function () {
+      $(this.hash).hide();
+    });
+
+    $(this).on('click', 'a', function(e){
+      $active.removeClass('active');
+      $content.hide();
+
+      $active = $(this);
+      $content = $(this.hash);
+
+      $active.addClass('active');
+      $content.show();
+      e.preventDefault();
+    });
+  });
+
+
+});
